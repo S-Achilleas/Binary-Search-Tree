2aaaa73a9ff9df80da2b7bf34587b4a0592cec7f
S-Achilleas <sarraxilleas@gmail.com>
2025-02-15 13:42:21 +0200

    Implemented the WordFrequency class which keeps track of the frequency of each word

---
+++ WordFrequency.java
@@ -0,0 +1,21 @@
+public class WordFrequency {
+    private Key wordKey;
+    private int frequency;
+
+    public WordFrequency(String word) {
+        this.wordKey = new Key(word);
+        this.frequency = 1;
+    }
+
+    public void incrementFrequency() {
+        this.frequency++;
+    }
+
+    public Key key(){
+        return wordKey;
+    }
+
+    public String toString() {
+        return wordKey.getWord() + ": " + frequency;
+    }
+}
