efbc049a9aa46cdf359bab0f5611d6e21af7169e
S-Achilleas <sarraxilleas@gmail.com>
2025-02-15 13:34:51 +0200

    Implemented the key class

---
+++ Key.java
@@ -0,0 +1,21 @@
+public class Key {
+    private String word;
+
+    public Key(String key) {
+        this.word = key;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public boolean equals(Key w) {
+        return this.word.equals(w.getWord());
+    }
+
+
+    public boolean lessThan(Key w) {
+        return  this.word.compareTo(w.getWord()) < 0;
+    }
+}
+
