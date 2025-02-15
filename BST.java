79747124f3982bfbe44ba27ebeeefec255643412
S-Achilleas <sarraxilleas@gmail.com>
2025-02-15 13:49:52 +0200

    Added the BST.java file

---
+++ BST.java
@@ -0,0 +1,69 @@
+import java.io.PrintStream;
+
+public class BST implements  WordCounter{
+
+    @Override
+    public void insert(String w) {
+
+    }
+
+    @Override
+    public WordFrequency search(String w) {
+        return null;
+    }
+
+    @Override
+    public void remove(String w) {
+
+    }
+
+    @Override
+    public void load(String filename) {
+
+    }
+
+    @Override
+    public int getNumTotalWords() {
+        return 0;
+    }
+
+    @Override
+    public int getNumDistinctWords() {
+        return 0;
+    }
+
+    @Override
+    public int getFrequency(String w) {
+        return 0;
+    }
+
+    @Override
+    public WordFrequency getMaxFrequency() {
+        return null;
+    }
+
+    @Override
+    public double getMeanFrequency() {
+        return 0;
+    }
+
+    @Override
+    public void addStopWord(String w) {
+
+    }
+
+    @Override
+    public void removeStopWord(String w) {
+
+    }
+
+    @Override
+    public void printTreeByWord(PrintStream stream) {
+
+    }
+
+    @Override
+    public void printΤreeByFrequency(PrintStream stream) {
+
+    }
+}
