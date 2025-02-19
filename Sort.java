537e63507eb1cb1a9c1e37cdcf7ec90a9ac47b79
Achilleas <sarraxilleas@gmail.com>
2025-02-19 16:36:12 +0200

    implemented the quicksort

---
+++ Sort.java
@@ -0,0 +1,30 @@
+public class Sort {
+
+    // quicksort
+    public static void quicksort(WordFrequency[] array, int p, int r) {
+        if (r <= p) return;
+        int i = partition(array, p, r);
+        quicksort(array, p, i-1);
+        quicksort(array, i+1, r);
+    }
+
+    public static int partition(WordFrequency array[], int p, int r){
+        int i = p-1, j = r;
+        WordFrequency v = array[r];
+
+        for (;;){
+            while (!(array[++i].getKey().less( v.getKey())));
+            while (!(v.getKey().less(array[--j].getKey())))
+                if (j == p) break;
+            if (i >= j) break;
+            swap(array, i, j);
+        }
+        swap(array, i, r);
+        return i; }
+
+    private static void swap(WordFrequency[] array, int i, int j) {
+        WordFrequency temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
